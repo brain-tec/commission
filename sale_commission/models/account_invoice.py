bb36c74ee8f2d381be6cc97ea180e628e68c5937
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-03-24 20:31:56 +0100

    [FIX] sale_commission: No commission should be populated on vendor bills
    (cherry picked from commit 41a90c4d8654c1284484602b55bf56fa436d9884)
    
    Conflicts:
    
    - sale_commission/models/account_invoice.py: def create(self, vals)
      used self._prepare_agents_vals instead of self._prepare_agents_vals_partner
    
    - sale_commission/models/account_invoice.py: def _prepare_agents_vals(self)
      add local invoice (I guess this is for when _prepare_agents_vals isn't called
      from within create), then checked if invoice.type[:3] == 'out': (patch used 'inv')

--- sale_commission/models/account_invoice.py
+++ sale_commission/models/account_invoice.py
@@ -70,6 +70,13 @@ class AccountInvoiceLine(models.Model):
         compute="_compute_any_settled",
     )
 
+    @api.model
+    def _default_agents(self):
+        """Don't populate agents for supplier invoices."""
+        if self.env.context.get('type', '')[:2] == 'in':
+            return []
+        return super()._default_agents()
+
     @api.depends('agents', 'agents.settled')
     def _compute_any_settled(self):
         for record in self:
@@ -83,17 +90,23 @@ class AccountInvoiceLine(models.Model):
         invoice_id = vals.get('invoice_id', False)
         if (agents_vals and agents_vals[0][0] == 6 and not
                 agents_vals[0][2] and invoice_id):
-            vals['agents'] = self._prepare_agents_vals(vals=vals)
+            inv = self.env['account.invoice'].browse(invoice_id)
+            vals['agents'] = (
+                self._prepare_agents_vals(vals=vals)
+                if inv.type[:3] == 'out' else [(6, 0, [])])
         return super().create(vals)
 
     def _prepare_agents_vals(self, vals=None):
         res = super()._prepare_agents_vals(vals=vals)
         if self:
+            invoice = self.invoice_id
             partner = self.invoice_id.partner_id
         else:
             invoice = self.env['account.invoice'].browse(vals['invoice_id'])
             partner = invoice.partner_id
-        return res + self._prepare_agents_vals_partner(partner)
+        if invoice.type[:3] == 'out':
+            res += self._prepare_agents_vals_partner(partner)
+        return res
 
 
 class AccountInvoiceLineAgent(models.Model):
